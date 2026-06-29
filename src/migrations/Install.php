dc82b5f679d3546eb6cc3820e3d6a58c10afd956
Pete Eveleigh <pete@fostercommerce.com>
2026-06-29 13:29:06 +0100

    revert change to install migration

--- src/migrations/Install.php
+++ src/migrations/Install.php
@@ -11,7 +11,7 @@ class Install extends Migration
 	{
 		$this->createTable(Coupon::TABLE_NAME, [
 			'id' => $this->primaryKey(),
-			'name' => $this->string()->notNull(),
+			'title' => $this->string()->notNull(),
 			'code' => $this->string()->null(),
 			'triggerCondition' => $this->json()->null(),
 			'actionCondition' => $this->json()->null(),
