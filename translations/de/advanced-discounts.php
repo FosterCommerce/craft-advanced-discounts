36ff46c8b6d4a35631dd6b54a5ff1d6dd81768ec
Pete Eveleigh <pete@fostercommerce.com>
2026-07-20 12:02:53 +0100

    update language files

--- translations/de/advanced-discounts.php
+++ translations/de/advanced-discounts.php
@@ -5,6 +5,7 @@ return [
     'AND' => 'UND',
     'OR' => 'ODER',
     'Add an action' => 'Aktion hinzufügen',
+    'Add a message' => 'Nachricht hinzufügen',
     'Date Range' => 'Datumsbereich',
     'From' => 'Von',
     'To' => 'Bis',
@@ -13,6 +14,8 @@ return [
     'User' => 'Benutzer',
     'Related To' => 'In Beziehung zu',
     'Specific Purchasable' => 'Bestimmtes Produkt',
+    'Quantity' => 'Menge',
+    'Any qty' => 'Beliebige Menge',
     'Shipping Method' => 'Versandmethode',
     'Line Items' => 'Artikelpositionen',
     'All line items' => 'Alle Artikelpositionen',
@@ -20,12 +23,20 @@ return [
 
     // Felder der Rabattregel
     'Apply to' => 'Anwenden auf',
+    'Apply per' => 'Anwenden pro',
+    'Per line item' => 'Pro Artikelposition',
+    'Per purchasable' => 'Pro Produkt',
     'Discount Type' => 'Rabatttyp',
     'Discount value' => 'Rabattwert',
     'Flat Amount' => 'Festbetrag',
     'Percentage' => 'Prozentsatz',
     'Discount a flat amount' => 'Einen Festbetrag abziehen',
     'Discount a percentage' => 'Einen Prozentsatz abziehen',
+
+    // Felder der Nachrichtenregel
+    'Message' => 'Nachricht',
+    'e.g. Spend another {amountRemaining} to get {discountAmount} off' => 'z. B. Geben Sie weitere {amountRemaining} aus, um {discountAmount} Rabatt zu erhalten',
+    'Create rules to determine when to show this message' => 'Erstellen Sie Regeln, um festzulegen, wann diese Nachricht angezeigt wird',
 
     // CP-Index
     'Advanced Discounts' => 'Erweiterte Rabatte',
@@ -43,8 +54,10 @@ return [
     'Leave blank to apply to every applicable order (like a sale). Entering a code requires the code to be entered during checkout for the discount to apply.' => 'Leer lassen, um den Rabatt auf jede zutreffende Bestellung anzuwenden (wie ein Angebot). Wenn ein Code angegeben wird, muss er an der Kasse eingegeben werden, damit der Rabatt gilt.',
     'Conditions' => 'Bedingungen',
     'Rules that decide when this discount applies.' => 'Regeln, die festlegen, wann dieser Rabatt gilt.',
-    'Action Rules' => 'Aktionsregeln',
-    'Create rules to apply discounts.' => 'Erstellen Sie Regeln zum Anwenden von Rabatten.',
+    'Actions' => 'Aktionen',
+    'Create actions to apply when the customer matches the rules above.' => 'Erstellen Sie Aktionen, die angewendet werden, wenn der Kunde die obigen Regeln erfüllt.',
+    'Messages' => 'Nachrichten',
+    'Create messages to show to customers, when they match certain conditions.' => 'Erstellen Sie Nachrichten, die Kunden angezeigt werden, wenn sie bestimmte Bedingungen erfüllen.',
 
     // Validierungsfehler
     'At least one condition is required.' => 'Es ist mindestens eine Bedingung erforderlich.',
--- translations/en-US/advanced-discounts.php
+++ translations/en-US/advanced-discounts.php
@@ -5,6 +5,7 @@ return [
     'AND' => 'AND',
     'OR' => 'OR',
     'Add an action' => 'Add an action',
+    'Add a message' => 'Add a message',
     'Date Range' => 'Date Range',
     'From' => 'From',
     'To' => 'To',
@@ -13,6 +14,8 @@ return [
     'User' => 'User',
     'Related To' => 'Related To',
     'Specific Purchasable' => 'Specific Purchasable',
+    'Quantity' => 'Quantity',
+    'Any qty' => 'Any qty',
     'Shipping Method' => 'Shipping Method',
     'Line Items' => 'Line Items',
     'All line items' => 'All line items',
@@ -20,12 +23,20 @@ return [
 
     // Discount rule fields
     'Apply to' => 'Apply to',
+    'Apply per' => 'Apply per',
+    'Per line item' => 'Per line item',
+    'Per purchasable' => 'Per purchasable',
     'Discount Type' => 'Discount Type',
     'Discount value' => 'Discount value',
     'Flat Amount' => 'Flat Amount',
     'Percentage' => 'Percentage',
     'Discount a flat amount' => 'Discount a flat amount',
     'Discount a percentage' => 'Discount a percentage',
+
+    // Message rule fields
+    'Message' => 'Message',
+    'e.g. Spend another {amountRemaining} to get {discountAmount} off' => 'e.g. Spend another {amountRemaining} to get {discountAmount} off',
+    'Create rules to determine when to show this message' => 'Create rules to determine when to show this message',
 
     // CP index
     'Advanced Discounts' => 'Advanced Discounts',
@@ -43,8 +54,10 @@ return [
     'Leave blank to apply to every applicable order (like a sale). Entering a code requires the code to be entered during checkout for the discount to apply.' => 'Leave blank to apply to every applicable order (like a sale). Entering a code requires the code to be entered during checkout for the discount to apply.',
     'Conditions' => 'Conditions',
     'Rules that decide when this discount applies.' => 'Rules that decide when this discount applies.',
-    'Action Rules' => 'Action Rules',
-    'Create rules to apply discounts.' => 'Create rules to apply discounts.',
+    'Actions' => 'Actions',
+    'Create actions to apply when the customer matches the rules above.' => 'Create actions to apply when the customer matches the rules above.',
+    'Messages' => 'Messages',
+    'Create messages to show to customers, when they match certain conditions.' => 'Create messages to show to customers, when they match certain conditions.',
 
     // Validation errors
     'At least one condition is required.' => 'At least one condition is required.',
